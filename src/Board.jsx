0a870736e9f616b82ab5c9aa9ed54359a334bf8e
Ymir <36711026+Ymirke@users.noreply.github.com>
2020-07-03 11:59:17 +0200

    fixed bug

--- src/Board.jsx
+++ src/Board.jsx
@@ -6,27 +6,17 @@ export default class Board extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: 'hello',
+      data: '',
     };
   }
 
-  getData = () => {
+  componentDidMount() {
     fetch('https://api.covid19api.com/summary')
       .then((res) => res.json())
       .then((result) => {
-        this.setState({ data: result });
+        this.setState({ data: result.Global.TotalConfirmed });
         console.log(this.state);
       });
-  };
-
-  componentDidMount() {
-    this.getData();
-  }
-
-  componentDidUpdate(prevProps) {
-    if (false) {
-      this.getData();
-    }
   }
 
   render() {
