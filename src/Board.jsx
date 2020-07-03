d05f33b40457c612bbef6a9b90839a6a08c9c145
Ymir <36711026+Ymirke@users.noreply.github.com>
2020-07-03 11:03:18 +0200

    making getData function

--- src/Board.jsx
+++ src/Board.jsx
@@ -3,9 +3,29 @@ import Card from './Card';
 import './Board.css';
 
 export default class Board extends Component {
+  constructor(props) {
+    super(props);
+    state = {
+      data: null,
+    }
+  }
+
+  getData = () => {
+    fetch('https://api.covid19api.com/summary')
+      .then((res) => {
+        res.json
+      }).then(data => this.setState({ data }))
+  }
+
+  componentDidMount() {
+    this.getData();
+  }
+
   render() {
     return (
       <main className="board">
+
+
         <Card />
       </main>)
   }
--- src/Card.jsx
+++ src/Card.jsx
@@ -2,10 +2,15 @@ import React, { Component } from 'react'
 import './Card.css'
 
 export default class Card extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
-      <article className='card'>
-        <h1 className='title'>Title</h1>
+      <article className="card">
+        <h1 className="title">Title</h1>
+        <h2 className="data">{props.value}</h2>
       </article>
     )
   }
