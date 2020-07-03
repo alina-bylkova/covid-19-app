b9ab16bc7f08c9aca6627b3fcc617f289b3b2ab4
Markus W. Myrland <markusmyrland@hotmail.com>
2020-07-03 13:20:26 +0200

    added select dropdown

--- src/Countries.jsx
+++ src/Countries.jsx
@@ -3,28 +3,38 @@ import React, { Component } from 'react';
 export default class Countries extends Component {
   constructor(props) {
     super(props);
-    state = {
+    this.state = {
       countryData: null,
       countries: null,
     }
   }
+  componentDidMount() {
 
-  getCountries = function () {
-    fetch('https://api.covid19api.com/countries')
-      .then((res) => res.json())
-      .then(data => this.setState({ countries: data }))
+    this.getCountries = function () {
+      console.log('this is running')
+      fetch('https://api.covid19api.com/countries')
+        .then((res) => res.json())
+        .then(data => this.setState({ countries: data }))
+    }
+    this.getCountryData = function (country) {
+      fetch(`https://api.covid19api.com/total/country/${country}`)
+        .then((res) => res.json())
+        .then(data => this.setState({ countryData: data }))
+    }
   }
 
-  getCountryData = function (country) {
-    fetch(`https://api.covid19api.com/total/country/${country}`)
-      .then((res) => res.json())
-      .then(data => this.setState({ countryData: data }))
-  }
 
   render() {
     return (
       <form>
-        <input type="dropdown"></input>
+        <label for="countries">Choose a country</label>
+        <select id="countries">
+          {
+            this.state.countries.map((element) => {
+              return (<option value={element.country} key={element.country}>{element.country}</option>)
+            })
+          }
+        </select>
       </form>
     );
   }
